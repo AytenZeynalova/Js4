e72692ea202f4e8f45a648ec8532314277a9d650
AytenZeynalova <14rvxpg@code.edu.az>
2022-11-30 01:08:35 +0400

    updated changes

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,94 +1,102 @@
-// task1-1
+// // task1-1
 
-let arr = [5,3,8,22,77,2,7,1,90,45,32,11]
+// let arr = [5,3,8,22,77,2,7,1,90,45,32,11]
 
 
 
-function FindMinimumOfArr (array){
-    let min=array[0];
+// function FindMinimumOfArr (array){
+//     let min=array[0];
 
-    for (let i = 0; i < array.length; i++) 
-    {
+//     for (let i = 0; i < array.length; i++) 
+//     {
     
-        if(array[i]<min){
+//         if(array[i]<min){
     
-            min=array[i]
+//             min=array[i]
     
-        }
+//         }
     
-    }
+//     }
     
-    console.log(`minimum digit: ${min}`)
-}
+//     console.log(`minimum digit: ${min}`)
+// }
 
-FindMinimumOfArr(arr)
+// FindMinimumOfArr(arr)
 
-// task1-2
+// // task1-2
 
-function FindMaximumOfArr (array){
+// function FindMaximumOfArr (array){
     
-let max=array[0];
+// let max=array[0];
 
-for (let i = 0; i < array.length; i++) 
-{
+// for (let i = 0; i < array.length; i++) 
+// {
 
-    if(array[i]>max){
+//     if(array[i]>max){
 
-        max=array[i]
+//         max=array[i]
 
-    }
+//     }
 
-}
+// }
 
-console.log(`maximum digit: ${max}`)
-}
+// console.log(`maximum digit: ${max}`)
+// }
 
-FindMaximumOfArr(arr)
-
-
-// task1_deepEqual
-
-const cat={
-    name:'tom',
-    age:3,
-    owner:'aytan',
-    breed:'female'
-}
-
-const dog={
-    name:'lucy',
-    age:3,
-    owner:'aytan',
-    breed:'male'
-}
+// FindMaximumOfArr(arr)
 
 
-function DeepEqual(obj1,obj2){
+// // task1_deepEqual
 
-    let obj1Length=Object.keys(obj1).length;
-    let obj2Length=Object.keys(obj2).length;
+// const cat={
+//     name:'tom',
+//     age:3,
+//     owner:'aytan',
+//     breed:'female'
+// }
 
-    if(obj1===obj2){
-        return true;
-    }
-    else if ((typeof obj1 == "object" && obj1 != null) && (typeof obj2 == "object" && obj2 != null)) {
-        if (obj1Length != obj2Length){
-            return false;
-        }
+// const dog={
+//     name:'lucy',
+//     age:3,
+//     owner:'aytan',
+//     breed:'male'
+// }
+
+
+
+
+// function DeepEqual(obj1,obj2){
+
+//     let obj1Length=Object.keys(obj1).length;
+//     let obj2Length=Object.keys(obj2).length;
+
+
+//     let obj1type=typeof obj1;
+//     let obj2type=typeof obj2;
+
+//     if(obj1===obj2){
+//         return true;
+//     }
+//     else if ((obj1type == "object" && obj1 != null) && (obj2type == "object" && obj2 != null)) {
+//         if (obj1Length != obj2Length){
+//             return false;
+//         }
     
-        for (var z in obj1) {
-          if (obj2.hasOwnProperty(z))
-          {  
-            if (! DeepEqual(obj1[z], obj2[z]))
-              return false;
-          }
-          else
-            return false;
-        }
+//         for (var property in obj1) {
+//           if (obj2.hasOwnProperty(property))
+//           {  
+//             if (! DeepEqual(obj1[property], obj2[property]))
+//               return false;
+//           }
+//           else
+//             return false;
+//         }
         
-        return true;
-      }
-      else 
-        return false;
-  }
+//         return true;
+//       }
+//       else 
+//         return false;
+//   }
   
+//   console.log(DeepEqual(cat,dog))
+
